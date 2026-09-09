8bb9080b5604db31adef5b67e2006dccb29ef380
Devni Dharmasooriya <153256670+daveasda@users.noreply.github.com>
2026-09-09 15:14:19 +0530

    Checking constants

--- backend/server.js
+++ backend/server.js
@@ -13,10 +13,10 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-  origin: 'http://localhost:5173',  // Your React app's URL
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
   credentials: true
 }));
-const port=process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
